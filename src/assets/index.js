4e701431c25ac3d4cd29798dade0ddc7da7cc3d7
felixNyalenda <felixasejunior@gmail.com>
2024-01-14 00:21:31 +0300

    complete setup process scetion

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,17 @@
-import heroBg from "./hero-bg.png";
-import mobile from "./mobile.png";
-import watch from "./watch.svg";
-import referral from "./referral.svg";
-import safety from "./safety.svg";
-import dollar from "./dollar.svg";
-import identity from "./identity.svg";
-import startExchanging from "./start-exchanging.svg";
-import transferMoney from "./transfer-money.svg";
-import createAccount from "./create-account.svg";
+import heroBg from "./landing-page/hero-bg.png";
+import mobile from "./landing-page/mobile.png";
+import watch from "./landing-page/watch.svg";
+import referral from "./landing-page/referral.svg";
+import safety from "./landing-page/safety.svg";
+import dollar from "./landing-page/dollar.svg";
+import identity from "./landing-page/identity.svg";
+import startExchanging from "./landing-page/start-exchanging.svg";
+import transferMoney from "./landing-page/transfer-money.svg";
+import createAccount from "./landing-page/create-account.svg";
+import phone from "./landing-page/phone.png";
+import appleStore from "./landing-page/apple-store.svg";
+import googlePlay from "./landing-page/google-play.svg";
+import dot from "./landing-page/dot.svg";
 
 export {
   heroBg,
@@ -20,4 +24,8 @@ export {
   startExchanging,
   transferMoney,
   createAccount,
+  phone,
+  appleStore,
+  googlePlay,
+  dot,
 };
